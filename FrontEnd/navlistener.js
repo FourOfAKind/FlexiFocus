8194590e9ce0b62629e1b9f81fb271d1798efc62
FourOfAKind <mrjamshodgson@gmail.com>
2023-09-14 20:54:01 +0100

    add skeleton to original mode

--- FrontEnd/navlistener.js
+++ FrontEnd/navlistener.js
@@ -1,30 +1,36 @@
 const pomodoroButton = document.querySelector('.pomodoro-btn');
 const stopwatchButton = document.querySelector('.stopwatch-btn');
+const originalButton = document.querySelector('.original-btn');
 const unselectedDiv = document.querySelector('.unselected');
 const pomodoroDiv = document.querySelector('.pomodoro');
 const stopwatchDiv = document.querySelector('.stopwatch');
+const originalDiv = document.querySelector('.original');
 const bgColor = document.querySelector('body');
 const activeColor = getComputedStyle(document.documentElement).getPropertyValue('--active');
 const inactiveColor = getComputedStyle(document.documentElement).getPropertyValue('--inactive');
 
-pomodoroButton.addEventListener('click', function() {
-  try {
-  clearInterval(timer); 
-  } catch (error) {
-    console.log("Timer not started yet");
-  }
-  pomodoroDiv.style.display = 'flex';
-  stopwatchDiv.style.display = 'none';
-  unselectedDiv.style.display = 'none';
-});
-
-stopwatchButton.addEventListener('click', function() {
+function hideModes(remainingDiv) {
   try {
     clearInterval(timer); 
     } catch (error) {
-      console.log("Timer not started yet");
+      // No action needed - catch is left blank
     }
+  bgColor.style.backgroundColor = inactiveColor;
   pomodoroDiv.style.display = 'none';
-  stopwatchDiv.style.display = 'flex';
+  stopwatchDiv.style.display = 'none';
+  originalDiv.style.display = 'none';
   unselectedDiv.style.display = 'none';
+  remainingDiv.style.display = 'flex';
+}
+
+pomodoroButton.addEventListener('click', function() {
+  hideModes(pomodoroDiv);
+});
+
+stopwatchButton.addEventListener('click', function() {
+  hideModes(stopwatchDiv);
+});
+
+originalButton.addEventListener('click', function() {
+  hideModes(originalDiv);
 });
