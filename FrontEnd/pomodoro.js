853c724253fdaf7d83a98c3a60107bc008468d71
FourOfAKind <mrjamshodgson@gmail.com>
2023-09-17 16:40:14 +0100

    fix time date inconsistencies

--- FrontEnd/pomodoro.js
+++ FrontEnd/pomodoro.js
@@ -2,7 +2,7 @@ const pStartStopButton = document.querySelector(".pomodoro-start-stop-btn");
 const pSkipButton = document.querySelector(".pomodoro-skip-btn");
 const pResetButton = document.querySelector(".pomodoro-reset-btn");
 
-let [pSeconds, pMinutes] = [0, 25];
+let [pSeconds, pMinutes, pHours] = [0, 25, 0];
 let pStartStop = 0;
 let pMode = "Pomodoro";
 let currentMode = 0;
@@ -12,15 +12,19 @@ function decrementTimer() {
     if (pSeconds < 0) {
         pMinutes--;
         pSeconds = 59;
+    } if (pMinutes < 0) {
+        pHours--;
+        pMinutes = 59;
     }
 
     let s = pSeconds < 10 ? `0${pSeconds}` : `${pSeconds}`;
     let m = pMinutes < 10 ? `0${pMinutes}` : `${pMinutes}`;
+    let h = pHours < 10 ? `0${pHours}` : `${pHours}`;
 
-    document.querySelector(".pomodoro-display").innerHTML = `${m}:${s}`;
-    document.title = `Active Pomodoro: ${m}:${s}`;
+    document.querySelector(".pomodoro-display").innerHTML = `${h}:${m}:${s}`;
+    document.title = `Active Pomodoro: ${h}:${m}:${s}`;
 
-    if(pMinutes < 0) {
+    if(pMinutes < 0 && pHours < 0) {
         currentMode++;
         checkMode(currentMode);
     }
